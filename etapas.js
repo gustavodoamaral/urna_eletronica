586fd21fd203f1f0e4af3b041dab8d7080cacf0f
Gustavo do Amaral <64814431+gustavodoamaral@users.noreply.github.com>
2021-10-25 21:54:27 -0300

    Urna Eletrônica

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR', 
+        numeros: 5, 
+        candidatos:[
+            {
+                numero: '34221', 
+                nome: 'Cássio Fernandes',
+                partido: 'ABC', 
+                fotos: [
+                    {url: '34221.jpeg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '86221',
+                nome: 'Catarina La Red',
+                partido: 'DEF',
+                fotos: [
+                    {url: '86221.jpeg', legenda: 'Vereador'}
+                ]
+
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Fábio Gutenberg',
+                partido: 'ABC',
+                vice: 'Luiza Sena',
+                fotos: [
+                    {url: '99.jpeg', legenda: 'Prefeito'},
+                    {url: '99.2.jpeg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84', 
+                nome: 'Carla Kadoce',
+                partido: 'XYZR',
+                vice: 'Ricardo Montes', 
+                fotos: [
+                    {url: '84.jpeg', legenda: 'Prefeito'},
+                    {url: '84.2.jpeg', legenda:'Vice Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
